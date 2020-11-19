ab3e7363f8cb0c660b7cd744c8308550ffbc7b6e
kiarras <kiarras.marinero@gmail.com>
2020-11-19 01:08:01 +0100

    Add saquare

---
+++ Chess/src/Square.java
@@ -0,0 +1,21 @@
+
+
+public class Square {
+
+	private Coordenate coordenate;
+	private char code;
+
+	public Square(Coordenate coordenate, char code) {
+		this.coordenate = coordenate;
+		this.code = code;
+	}
+
+	public Coordenate getCoordenate() {
+		return coordenate;
+	}
+
+	public char getCode() {
+		return code;
+	}
+
+}
